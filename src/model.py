ed08c83262053d806ee50e152589886c999aca19
Oskar <oskar.pau2@gmail.com>
2025-12-01 12:32:37 +0200

    com

--- src/model.py
+++ src/model.py
@@ -601,7 +601,39 @@ class Model:
                 active_indexes=active_indexes
             )
         return df_out
+    
+
 
     ########################################################
     # END OF MAIN FUNCTION FOR PREPROCESSING INPUT AND CALCULATING CONFIDENCE
     ########################################################
+
+def main():
+    """
+    This function loops over all datasets, their categories and subcategories
+    """
+
+    RESULTS_PATH = "initial_results.csv"
+
+    # TODO: Somehow loop over the datasets
+    datasets = [[1]]
+    results_df = pd.DataFrame()
+
+    model = Model()
+
+    for dataset in datasets:
+        for row in dataset:
+            dataframe_to_append = model.run_batch_and_compute_confidence(
+                dataset_name="dummy", 
+                categories = ["dummy_categorie"], 
+                subcategories = ["dummy_subcategorie"],
+                questions = ["What is equal to 2 + 2?"],
+                right_answers = [["4"]],
+                question_types = ["multi_str"]
+                )
+            results_df = pd.concat([results_df, dataframe_to_append])
+    
+    results_df.to_csv(RESULTS_PATH, index=False, sep=";")
+
+if __name__ == "__main__":
+    main()
